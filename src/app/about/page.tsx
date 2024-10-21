35e446d36f9463c44b2dbc67f98fbaa0906beb78
kj-Empiric <keyur@empiricinfotech>
2024-10-21 17:29:16 +0530

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+    return <div> <h1 className='font-bold text-black text-center mt-10'>About page</h1></div>;
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className='font-bold text-black text-center mt-10'>Home page</h1>
+    </div>
+  );
+}
